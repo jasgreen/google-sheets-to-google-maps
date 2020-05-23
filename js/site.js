bb4efe3ad00dd048153e552bc5e7bf7a0d42a571
Jason Green <jasongreen@Jasons-MacBook-Pro.local>
2020-05-23 23:58:19 +0100

    Added to the marker popup, changed colours to research area

--- js/site.js
+++ js/site.js
@@ -2,34 +2,87 @@ var map;
 var locations = [];
 
 function initialiseMap() {
-
-  https://docs.google.com/spreadsheets/d/1-BT1dAzjt--Z9e62jK6P-duZZC_MdVuwhJo8QIihL-Y/edit?ts=5ec97a28&pli=1#gid=1278703176
-
-  // Load data from an example Google spreadsheet that contains latitude and longitude columns using Google Sheets API v4 that returns JSON.
-  // Replace the ID of your Google spreadsheet and you API key in the URL:
-  // https://sheets.googleapis.com/v4/spreadsheets/ID_OF_YOUR_GOOGLE_SPREADSHEET/values/Sheet1!A2:Q?key=YOUR_API_KEY
-  // Also make sure your API key is authorised to access Google Sheets API - you can enable that through your Google Developer console.
-  // Finally, in the URL, fix the sheet name and the range that you are accessing from your spreadsheet. 'Sheet1' is the default name for the first sheet.
-  $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/1-BT1dAzjt--Z9e62jK6P-duZZC_MdVuwhJo8QIihL-Y/values/Town%20Master!B2:N?key=AIzaSyAMjGnUY0SonztgIaWqRBdeOWfM0Fx1CVY", function(data) {
-    	// data.values contains the array of rows from the spreadsheet. Each row is also an array of cell values.
-    	// Modify the code below to suit the structure of your spreadsheet.
+  $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/1-BT1dAzjt--Z9e62jK6P-duZZC_MdVuwhJo8QIihL-Y/values/Town%20Master!B2:X?key=AIzaSyAMjGnUY0SonztgIaWqRBdeOWfM0Fx1CVY", function(data) {
     	$(data.values).each(function() {
     		var location = {};
 				location.title = this[0];
 				location.alternativeName = this[2];
+				location.researchArea = this[19];
 				location.legacyName = this[3];
 				location.subregion = this[12];
+				location.region = this[10];
 				location.latitude = parseFloat(this[7]);
         location.longitude = parseFloat(this[8]);
-	  		locations.push(location);
+        location.areaCoordinator = this[18] || '';
+        location.townLeader = this[22] || '';
+        locations.push(location);
     	});
+
+      var mapStyle = [
+        {
+          "featureType": "administrative",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "landscape",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "transit",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        }
+      ];
 
       // Center on (0, 0). Map center and zoom will reconfigure later (fitbounds method)
       var mapOptions = {
+        mapTypeControlOptions: {
+          mapTypeIds: ['mapStyle', google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.TERRAIN]
+        },
+        mapTypeId: 'mapStyle',
         zoom: 7,
         center: new google.maps.LatLng(51.9194, 19.1451)
       };
       var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      map.mapTypes.set('mapStyle', new google.maps.StyledMapType(mapStyle, { name: 'Custom Map Style' }));
+
       setLocations(map, locations);
   });
 }
@@ -60,9 +113,10 @@ function createMarker(map, location, infowindow) {
     map: map,
     title: location.title,
   });
+
   marker.setIcon({
       path: google.maps.SymbolPath.CIRCLE,
-      fillColor:stringToColour(location.subregion),
+      fillColor:stringToColour(location.researchArea.replace(/aeiou/i, '')),
       fillOpacity: 1,
       strokeColor: 'white',
       strokeWeight: 1,
@@ -70,10 +124,14 @@ function createMarker(map, location, infowindow) {
   });
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.setContent('<div>'+
-    '<p><strong>Town</strong> ' + location.title + '</p>' +
-    '<p><strong>Alternative Name</strong> ' + location.alternativeName + '</p>' +
-    '<p><strong>Legacy Name</strong> ' + location.legacyName + '</p>' +
-    '<p><strong>Subregion Name</strong> ' + location.subregion + '</p>' +
+    '<p><strong>Primary Town Name</strong><br/> ' + location.title + '</p>' +
+    '<p><strong>Research Area</strong><br/> ' + location.researchArea + '</p>' +
+    '<p></p>' +
+    '<p><strong>Subregion</strong><br/> ' + location.subregion + '</p>' +
+    '<p><strong>Region</strong><br/> ' + location.region + '</p>' +
+    '<p></p>' +
+    '<p><strong>Area Coordinator</strong><br/> ' + location.areaCoordinator + '</p>' +
+    '<p><strong>Town Leader</strong><br/> ' + location.townLeader + '</p>' +
     '</div>');
     infowindow.open(map, marker);
   });
